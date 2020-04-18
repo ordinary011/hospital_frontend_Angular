2dc725e4f7055092d4a3da3c2468476435a96b76
liubomyr <debryniuk@gmail.com>
2020-04-18 23:25:54 +0300

    renamed server file

--- dist/angular_server.js
+++ dist/angular_server.js
@@ -6,6 +6,6 @@ app.use(express.static('./client'));
 
 app.get("/", (req, res, next) => {
   res.sendFile("./client/index.html");
-})
+});
 
 app.listen(4200, () => console.log("listenming on port 4200"));
