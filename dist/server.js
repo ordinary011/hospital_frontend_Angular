90bca1a296591f3d0b99002a138c5a0268bc89ca
liubomyr <debryniuk@gmail.com>
2020-04-18 22:31:58 +0300

    build is included for aws

---
+++ dist/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./client'));
+
+app.get("/", (req, res, next) => {
+  res.sendFile("./client/index.html");
+})
+
+app.listen(4200, () => console.log("listenming on port 4200"));
